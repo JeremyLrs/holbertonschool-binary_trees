85b86ac29c465059779de2f6e4bd82df668ed7ab
JeremyLrs <hello@laurensjeremy.com>
2025-08-06 10:19:50 +0200

    Modification of all the code and logic

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,34 +5,24 @@
  * @tree: Tree to measure
  * Return: height of the tree
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	/*Déclaration des variables pour stocker les hauteurs des sous-arbres g&d*/
-	size_t left_height, right_height;
+	size_t heightLeft = 0;
+	size_t heightRight = 0;
 
-	/*Si l'arbre est NULL ou s'il s'agit d'une feuille, la hauteur est 0*/
-	if (!tree || (!tree->left && !tree->right))
-	{
+	if (tree == NULL)
 		return (0);
-	}
 
-	/*Appel récursif pour le sous-arbre gauche. +1 pour le nœud courant*/
-	left_height = binary_tree_height(tree->left) + 1;
-	/*Appel récursif pour le sous-arbre droit. +1 pour le nœud courant*/
-	right_height = binary_tree_height(tree->right) + 1;
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-	/*Comparaison des hauteurs des deux sous-arbres*/
-	/*But: trouver la hauteur la + élevée pour déterminer la hauteur totale*/
-	if (left_height > right_height)
-	{
-		/*Si la hauteur gauche est supérieure, on la retourne*/
-		return (right_height);
-	}
+	/* Counting height of each child */
+	heightLeft += binary_tree_height(tree->left);
+	heightRight += binary_tree_height(tree->right);
 
+	/* Returning only the highest */
+	if (heightLeft > heightRight)
+		return (heightLeft + 1);
 	else
-	{
-		/*Retourne la hauteur droite (ou la hauteur gauche si elles sont égales)*/
-		return (left_height);
-	}
+		return (heightRight + 1);
 }
