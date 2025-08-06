1e37d2bc06f817fc041584b33c95146ba9e179ed
JeremyLrs <hello@laurensjeremy.com>
2025-08-06 10:24:14 +0200

    Modification of all the code and logic

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,25 +12,22 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int heightLeft = 0;
+	int heightRight = 0;
 
-	int left, right;
+	if (tree != NULL)
+	{
+		/* Measuring the height of each child */
+		heightLeft = binary_tree_height(tree->left);
+		heightRight = binary_tree_height(tree->right);
 
-	if (tree == NULL)
-		return (0);
+		if (tree->left == NULL)
+			heightLeft = -1;
 
-	if (tree->left != NULL)
-		left = 1 + (binary_tree_height(tree->left));
+		if (tree->right == NULL)
+			heightRight = -1;
+	}
 
-	else
-		left = 0;
-
-	if (tree->right != NULL)
-		right = 1 + (binary_tree_height(tree->right));
-	else
-		right = 0;
-
-	if (left > right)
-		return (left);
-	else
-		return (right);
+	/* Returning the difference of heights */
+	return (heightLeft - heightRight);
 }
